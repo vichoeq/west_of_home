a71b4966262a80e23b886f8cb73605d9ff97711c
Vicho <vichoeq@gmail.com>
2024-03-13 18:31:42 -0700

    All documents

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # West of Home
 My submission to the 2024 Acerola Game Jam
+
+# Synopsis
+
+Join our protagonist Belmuth on an epic journey that will take it far from home, to the forbidden lands of the west.
+
+# Controls
+
+Move: WASD
+Jump: W or space bar
+ 
+When bar is full, activate ability with left click
--- myscript.js
+++ myscript.js
@@ -1,37 +1,98 @@
 const levels = [
     {
-        not_a: "robot",
+        not_a: "a robot",
+        instructions: "Select all squares with",
+        lookfor: "traffic lights",
+        path: "imgs/traffic_light.jpg",
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 1, 0], [0, 0, 0, 0]]
+    },
+    {
+        not_a: "a robot",
         instructions: "Select all squares with",
         lookfor: "bicycles",
         path: "imgs/bike.jpg",
-        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [1, 0, 0, 0], [0, 0, 0, 0]]
     },
     {
-        not_a: "robot",
-        instructions: "Select all squares with",
-        lookfor: "buses",
-        path: "imgs/bus.jpg",
-        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    },
-    {
-        not_a: "dog",
+        not_a: "a dog",
         instructions: "Select all squares with",
         lookfor: "a red ball",
         path: "imgs/ball.jpg",
-        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[0, 0, 0, 0], [0, 0, 1, 1], [0, 0, 2, 2], [0, 0, 0, 0]]
     },
     {
-        not_a: "cat",
-        instructions: "Select all squares that",
-        lookfor: "don't call your attention",
+        not_a: "a cat",
+        instructions: "Select all squares with",
+        lookfor: "prey",
         path: "imgs/laser.jpg",
-        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
-    }
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[2, 2, 0, 2], [2, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]
+    },
+    {
+        not_a: "guilty of murder",
+        instructions: "Select the square with",
+        lookfor: "your alibi",
+        path: "imgs/alibi.jpg",
+        mode: "SELECT ONE",
+        error: "Please select one",
+    },
+    {
+        not_a: "an infectious disease",
+        instructions: "Select all squares with",
+        lookfor: "friends",
+        path: "imgs/white_blood_cell.jpg",
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[2, 2, 2, 2], [1, 2, 2, 2], [2, 2, 2, 2], [2, 2, 2, 2]]
+    },
+    {
+        not_a: "the one who killed her",
+        instructions: "",
+        lookfor: "Then who did it?",
+        path: "imgs/jimmy.jpg",
+        mode: "SELECT SOME",
+        error: "Who did it?",
+    },
+    {
+        not_a: "an artificial intelligence",
+        instructions: "Select all squares with",
+        lookfor: "blueberry muffins",
+        path: "imgs/chihuahua_muffin.jpg",
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[0, 0, 1, 0], [0, 0, 0, 0], [0, 1, 0, 0], [0, 0, 0, 0]]
+    },
+    {
+        not_a: "going to jail, am I?",
+        instructions: "Select all squares containing",
+        lookfor: "Jimmy's girlfriend",
+        path: "imgs/dumpster.jpg",
+        mode: "MATCH",
+        error: "You know where she is",
+        solution: [[0, 0, 0, 0], [0, 0, 2, 2], [0, 0, 1, 1], [0, 0, 1, 1]]
+    },
+    {
+        not_a: "a robot",
+        instructions: "Select all squares with",
+        lookfor: "crosswalks",
+        path: "imgs/crosswalk.jpg",
+        mode: "MATCH",
+        error: "Please select all the matching images",
+        solution: [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [1, 1, 1, 1]]
+    },
 ];
 
 var current_level = 0
 var selected = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
 var checkmark_clicked = false;
+var n_selected = 0;
 
 // Animated elements
 var checkbox;
@@ -40,6 +101,7 @@ var spinner;
 var checkmark;
 var image_select;
 var not_a_box;
+var next_button;
 
 // Animation metadata
 var checkbox_transition_frame;
@@ -56,6 +118,7 @@ function on_load() {
     checkmark = document.getElementById('recaptcha-checkbox-checkmark');
     image_select = document.getElementById('image-select');
     not_a_box = document.getElementById('recaptcha-anchor-label');
+    next_button = document.getElementById('recaptcha-verify-button');
 
     // CHECKBOX HOVER
     var checkbox_anchor = document.getElementById("recaptcha-anchor");
@@ -65,6 +128,7 @@ function on_load() {
     checkbox_anchor.addEventListener("mouseleave", function () {
         checkbox_anchor.classList.remove("recaptcha-checkbox-hover");
     });
+
 
     // IMAGE SELECT
     document.querySelectorAll('.rc-imageselect-tile').forEach(function (element) {
@@ -78,23 +142,32 @@ function on_load() {
 
 function launch_level() {
     if (current_level < levels.length) {
-        launch_checkbox();
         launch_image_select();
+        launch_checkbox();
     }
     else {
 
     }
 }
 
-function launch_checkbox() {
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function launch_checkbox() {
 
     // TODO ANIMATE TEXT CHANGE + BOX SIZE CHANGE
     if (current_level == 0) {
         not_a_box.innerText = levels[current_level].not_a;
     } else {
-        // checkmark_frame = 0;
-        // requestAnimationFrame(checkmark_animation_in_and_out);
-        launch_checkmark_animation_in_and_wait()
+        // If level has the same prompt as the previous one, jump to image select
+        if (levels[current_level].not_a == not_a_box.innerText) {
+            await sleep(1000);
+            image_select.style = "visibility: visible; opacity: 1";
+        }
+        else {
+            launch_checkmark_animation_in_and_wait()
+        }
     }
 }
 
@@ -108,14 +181,17 @@ function launch_image_select() {
     });
     // Reset selected (logical)
     selected = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]];
+    n_selected = 0;
+    next_button.innerText = "SKIP";
 
-    // Reset error message
+    // Reset error messages
     document.querySelector(".rc-imageselect-error-select-more").setAttribute("style", "display: none;");
 
     // LOAD
 
     // Load prompt for this level
     document.getElementById("look-for").innerText = levels[current_level].lookfor;
+    document.getElementById("instructions").innerText = levels[current_level].instructions;
 
     // Load image for this level
     document.querySelectorAll('.tile_image').forEach(function (element) {
@@ -130,33 +206,69 @@ function click_on_tile() {
 
     selected[row][col] = 1 - selected[row][col];
 
+    if (selected[row][col] == 1) {
+        n_selected++;
+        next_button.innerText = "NEXT";
+    } else {
+        n_selected--;
+        if (n_selected == 0) {
+            next_button.innerText = "SKIP";
+        }
+    }
+
     this.classList.toggle("rc-imageselect-tileselected");
 
-    console.log(selected);
+}
+
+function next_level() {
+    image_select.style = "visibility: hidden; opacity: 0; transition: none";
+
+    current_level++;
+
+    launch_level();
+}
+
+function error() {
+    document.getElementById("rc-imageselect-error-select-more").innerText = levels[current_level].error;
+    document.getElementById("rc-imageselect-error-select-more").setAttribute("style", "display: unset;");
 }
 
 /** NEXT button behaviour */
 function next() {
-    if (matrix_equals(selected, levels[current_level].solution)) {
+    if (levels[current_level].mode == "SELECT ONE") {
+        if (n_selected == 1) {
+            next_level();
+        }
+        else {
+            error();
+        }
 
-        // Stop spinner
-        spinner.style = "display: block; animation-play-state: paused; opacity: 0; transform: scale(1);"
-
-        image_select.style = "visibility: hidden; opacity: 0; transition: none";
-
-        current_level++;
-
-        launch_level();
     }
-    else {
-        document.querySelector(".rc-imageselect-error-select-more").setAttribute("style", "display: unset;");
+    else if (levels[current_level].mode == "MATCH") {
+        if (matrix_equals(selected, levels[current_level].solution)) {
+            next_level();
+        }
+        else {
+            error();
+        }
+
     }
+    else if (levels[current_level].mode == "SELECT SOME") {
+        if (n_selected > 0) {
+            next_level();
+        }
+        else {
+            error();
+        }
+    }
+
 }
 
-function matrix_equals(A, B) {
+function matrix_equals(selected, solution) {
     for (var row = 0; row < 4; row++) {
         for (var col = 0; col < 4; col++) {
-            if (A[row][col] != B[row][col]) return false;
+            if (solution[row][col] == 2) continue; // 2 means the actual value doesnt matter
+            if (selected[row][col] != solution[row][col]) return false;
         }
     }
     return true;
@@ -235,6 +347,8 @@ function checkmark_animation_in_and_wait(timestamp) {
 }
 
 function launch_checkmark_animation_in_and_wait() {
+    // Stop spinner
+    spinner.style = "display: block; animation-play-state: paused; opacity: 0; transform: scale(1);"
     checkmark_frame = 0;
     requestAnimationFrame(checkmark_animation_in_and_wait);
 }
